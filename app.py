9d15f92d4f2981e7ef9fcd650bb97e0571cb9cf1
David Shelton <david.faria012@gmail.com>
2024-01-26 01:42:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 load_dotenv(".env")
 
 app = Flask(__name__, template_folder="templates")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI1")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI2")
 
 # Inicia e configura o banco de dados
 db.init_app(app=app)
@@ -16,10 +16,6 @@ with app.test_request_context():
 
 # Register the usuario's blueprint
 app.register_blueprint(usuario_controller, url_prefix="/usuario/")
-
-#HOST = os.getenv("FLASK_HOST")
-HOST = "http://127.0.0.1:5000"
-
 
 
 if __name__ == "__main__":
